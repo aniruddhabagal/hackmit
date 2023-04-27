e2dd54112cf4cbeb9424dac28f5cf25fa786aad6
dhavansuresh7 <dhavansuresh07@gmail.com>
2023-04-27 18:58:26 +0530

    Added dashboard and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Dashboard from "./components/dashboard";
 import Quiz from "./components/quiz";
 import Tutorial from "./components/tutorial";
+import Login from "./components/login";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Login/>} />
+        <Route path="dashboard" element={<Dashboard />} />
         <Route path="quiz" element={<Quiz />} />
         <Route path="tutorial" element={<Tutorial />} />
       </Routes>
