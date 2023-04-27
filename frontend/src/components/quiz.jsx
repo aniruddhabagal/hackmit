6bb101a997508cbf3aa1c32031a42ef846373836
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-04-27 13:46:22 +0530

    Setup React Router

---
+++ frontend/src/components/quiz.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Quiz() {
+  return <h1>This is quiz</h1>;
+}
+
+export default Quiz;
---
+++ frontend/src/components/tutorial.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tutorial() {
+  return <h1>This is tutorial</h1>;
+}
+
+export default Tutorial;
