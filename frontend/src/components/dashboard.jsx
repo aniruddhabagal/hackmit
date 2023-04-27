9211ba0140bc2d8df5f2293a7a907c74958d68ea
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-04-28 03:23:20 +0530

    Added proper routes

--- frontend/src/components/dashboard.jsx
+++ frontend/src/components/dashboard.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import "./dashboard.css";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const navigateToTut = () => {
+    // 👇️ navigate to /contacts
+    navigate("/tutorial");
+  };
+
+  const navigateToQuiz = () => {
+    // 👇️ navigate to /contacts
+    navigate("/quiz");
+  };
+
   return (
     <div>
       <div className="logo-1"></div>
       <div className="speech-head">Parts Of Speech</div>
       <div className="cards">
         <div className="card tut">
-          <button className="btn-choose">Tutorial</button>
+          <button className="btn-choose" onClick={navigateToTut}>
+            Tutorial
+          </button>
         </div>
         <div className="card quiz">
-          <button className="btn-choose">Quiz</button>
+          <button className="btn-choose" onClick={navigateToQuiz}>
+            Quiz
+          </button>
         </div>
       </div>
     </div>
